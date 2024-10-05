73caac16422bf1e55965fa8f1b0ec30578191e3f
Ismaz999 <aziz.ism98@gmail.com>
2024-10-05 18:34:48 +0200

    Analyse des sentiments complexe

--- test_nlp.py
+++ test_nlp.py
@@ -4,15 +4,41 @@ import pandas as pd
 
 df_anime = pd.read_csv("anime_reviews/anime_reviews_2001_Tengen_Toppa_Gurren_Lagann.csv")
 
-classifier = pipeline("sentiment-analysis",truncation=True) 
+sentiment_classifier = pipeline("sentiment-analysis", truncation=True, max_length=512)
+emotion_classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", truncation=True, max_length=512, top_k=2)
 
-def analyze_anime(text, classifier):
-    resultat = classifier(text)
-    return resultat[0]['label']
+def split_text(text, max_tokens=512):
+    words = text.split()  
+    return [' '.join(words[i:i + max_tokens]) for i in range(0, len(words), max_tokens)]
 
-df_anime['sentiment'] = df_anime['review'].apply(lambda x: analyze_anime(x, classifier))
+def analyze_sentiment_and_emotions(text):
 
-print(df_anime.head())
+    segments = split_text(text)  
+    all_sentiments = []
+    all_emotions = []
+
+    for segment in segments:
+        sentiment_result = sentiment_classifier(segment)[0]['label']
+        all_sentiments.append(sentiment_result)
+        
+        emotion_result = emotion_classifier(segment)
+        top_emotions = {emotion['label']: emotion['score'] for emotion in emotion_result[0]}
+        all_emotions.append(top_emotions)
+
+    return ', '.join(all_sentiments), all_emotions     
+
+    # sentiment_result = sentiment_classifier(text)[0]['label']
+    
+    # emotion_result = emotion_classifier(text)
+    # top_emotions = {emotion['label']: emotion['score'] for emotion in emotion_result}
+    
+    # return sentiment_result, top_emotions
+
+df_anime[['sentiment', 'emotions']] = df_anime['review'].apply(lambda x: pd.Series(analyze_sentiment_and_emotions(x)))
+
+print(df_anime[['review', 'sentiment', 'emotions']].head())
+
+df_anime.to_csv("anime_reviews_with_sentiments_and_emotions.csv", sep=';', index=False)
 
 # classifier = pipeline("sentiment-analysis")
 # avis = "J'adore cet anime, les combats sont incroyables !"
